0e5b3d7b2b1e0f908c085c425928c57147ba544c
yurights <yuriibpk@gmail.com>
2025-03-18 13:44:56 +0200

    npm: tilewind added

--- apps/frontend/apps/anx-store/src/app/nx-welcome.component.ts
+++ apps/frontend/apps/anx-store/src/app/nx-welcome.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-nx-welcome',
   imports: [CommonModule, MatButtonModule],
@@ -12,10 +12,10 @@ import {MatButtonModule} from '@angular/material/button';
      Delete this file and get started with your project!
      * * * * * * * * * * * * * * * * * * * * * * * * * * * *
      -->
-   <h1>Hello world</h1>
-   <button mat-flat-button>Basic</button>
+    <h1 class="bg-blue-500">Hello world</h1>
+    <div class="flex items-center"><button mat-flat-button>Basic</button></div>
   `,
-  styles: [],
+  styles: ['h1, div, button {border: 1px solid red;}'],
   encapsulation: ViewEncapsulation.None,
 })
 export class NxWelcomeComponent {}
