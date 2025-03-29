154d90fbfdabc73883b5884f81ea4c93fe3ce6f9
yurights <yuriibpk@gmail.com>
2025-03-29 16:58:05 +0200

    feat: added header component

--- apps/frontend/apps/anx-store/src/app/app.component.ts
+++ apps/frontend/apps/anx-store/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { AppHeaderComponent } from './components/app-header/app-header.component';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, AppHeaderComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
