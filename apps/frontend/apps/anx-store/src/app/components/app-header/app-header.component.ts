d90339bf901276938cdf66f07e88c571974246c8
yurights <yuriibpk@gmail.com>
2025-04-04 11:49:29 +0300

    chore: removed unnecessary imports, rename yaml step

--- apps/frontend/apps/anx-store/src/app/components/app-header/app-header.component.ts
+++ apps/frontend/apps/anx-store/src/app/components/app-header/app-header.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-app-header',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './app-header.component.html',
-  styleUrl: './app-header.component.css',
 })
 export class AppHeaderComponent {}
