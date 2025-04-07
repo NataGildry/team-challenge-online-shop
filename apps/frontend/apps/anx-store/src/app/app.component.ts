cc1558b98640c1463279b49416594eefe6af1159
yurights <yuriibpk@gmail.com>
2025-04-07 10:42:55 +0300

    chore: shell lib created

--- apps/frontend/apps/anx-store/src/app/app.component.ts
+++ apps/frontend/apps/anx-store/src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { ShellComponent } from 'libs/shell/src/lib/shell/shell.component';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule, AppHeaderComponent],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    AppHeaderComponent,
+    ShellComponent,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
