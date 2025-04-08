a06ff63f44706a0681bc1ce2023831ddb9ad2291
yurights <yuriibpk@gmail.com>
2025-04-08 13:39:03 +0300

    chore: icon libs added

---
+++ apps/frontend/libs/shared/ui/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/person-icon/person-icon.component';
+export * from './lib/search-icon/search-icon.component';
+export * from './lib/basket-icon/basket-icon.component';
--- apps/frontend/libs/shell/src/lib/header/header.component.ts
+++ apps/frontend/libs/shell/src/lib/header/header.component.ts
@@ -2,11 +2,23 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { NavigationItemComponent } from '../navigation-item/navigation-item.component';
 import { RouterOutlet } from '@angular/router';
+import {
+  PersonIconComponent,
+  SearchIconComponent,
+  BasketIconComponent,
+} from '@anx-store/ui';
 
 @Component({
   selector: 'lib-header',
   standalone: true,
-  imports: [MatIconModule, NavigationItemComponent, RouterOutlet],
+  imports: [
+    MatIconModule,
+    NavigationItemComponent,
+    RouterOutlet,
+    PersonIconComponent,
+    SearchIconComponent,
+    BasketIconComponent,
+  ],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
