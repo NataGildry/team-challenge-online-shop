6ddaa1d93396135c59e86842d4f98f8ca702493b
yurights <yuriibpk@gmail.com>
2025-04-15 12:47:51 +0300

    chore: transloco installed

--- apps/frontend/apps/anx-store/src/app/app.config.ts
+++ apps/frontend/apps/anx-store/src/app/app.config.ts
@@ -1,15 +1,32 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  isDevMode,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { provideTransloco } from '@jsverse/transloco';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['en', 'ua'],
+        defaultLang: 'wn',
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
   ],
 };
---
+++ apps/frontend/apps/anx-store/transloco.config.ts
@@ -0,0 +1,9 @@
+import { TranslocoGlobalConfig } from '@jsverse/transloco-utils';
+
+const config: TranslocoGlobalConfig = {
+  rootTranslationsPath: 'src/public/i18n/',
+  langs: ['en', 'ua'],
+  keysManager: {},
+};
+
+export default config;
