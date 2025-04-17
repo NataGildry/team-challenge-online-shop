73c5ac95fbbef09d91a28291338905eda89121fa
Nataliia Podpriatova <Podpriatova@medialesson.de>
2025-04-17 18:01:55 +0200

    fix: revert key manager access

--- apps/frontend/transloco.config.ts
+++ apps/frontend/transloco.config.ts
@@ -2,7 +2,7 @@ import { TranslocoGlobalConfig } from '@jsverse/transloco-utils';
 
 const config: TranslocoGlobalConfig = {
   rootTranslationsPath: 'apps/anx-store/src/assets/i18n/',
-  langs: ['en'],
+  langs: ['en', 'uk'],
   keysManager: {},
 };
 
