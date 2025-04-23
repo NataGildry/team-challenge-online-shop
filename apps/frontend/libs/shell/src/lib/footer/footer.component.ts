6c328abfdba3fc1fbbd29bf195e463c9107a353f
yurights <yuriibpk@gmail.com>
2025-04-23 08:54:31 +0300

    chore: footer component added

--- apps/frontend/libs/shell/src/lib/footer/footer.component.ts
+++ apps/frontend/libs/shell/src/lib/footer/footer.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { TranslocoDirective, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'lib-footer',
-  imports: [CommonModule],
+  imports: [TranslocoDirective],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  private readonly translocoService: TranslocoService =
+    inject(TranslocoService);
+}
