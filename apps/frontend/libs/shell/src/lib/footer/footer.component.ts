06fd7cf391fd48d21e20937f634e9239986e75bf
yurights <yuriibpk@gmail.com>
2025-04-24 14:14:00 +0300

    chore: added translate

--- apps/frontend/libs/shell/src/lib/footer/footer.component.ts
+++ apps/frontend/libs/shell/src/lib/footer/footer.component.ts
@@ -1,14 +1,23 @@
 import { Component, inject } from '@angular/core';
+import {
+  IconComponent,
+  instagramLogo,
+  telegramLogo,
+  twitterLogo,
+} from '@anx-store/shared/ui';
 import { CapitalizeFirstWordPipe } from '@anx-store/shared/utils';
 import { TranslocoDirective, TranslocoService } from '@jsverse/transloco';
 
 @Component({
   selector: 'lib-footer',
-  imports: [TranslocoDirective, CapitalizeFirstWordPipe],
+  imports: [TranslocoDirective, CapitalizeFirstWordPipe, IconComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
 export class FooterComponent {
   private readonly translocoService: TranslocoService =
     inject(TranslocoService);
+  public readonly instagram = instagramLogo;
+  public readonly telegram = telegramLogo;
+  public readonly twitter = twitterLogo;
 }
