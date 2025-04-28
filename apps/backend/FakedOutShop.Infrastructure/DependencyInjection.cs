1be5384e76763c27ce4c68567d135eed043f96b9
MaxSeneca3 <naumovmaksim647@gmail.com>
2025-04-28 23:19:19 +0300

    Added user repository, interface, service. Modified the code (#33)
    * Added user repository, interface and service. Modified the code
    
    * Removed the get by unique email endpoint in user controller
    
    * Keeping the db configuration logic inside the CosmosClientProvider

--- apps/backend/FakedOutShop.Infrastructure/DependencyInjection.cs
+++ apps/backend/FakedOutShop.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,11 @@
 using FakedOutShop.Application.Abstractions;
 using FakedOutShop.Application.Models.Services;
+using FakedOutShop.Domain.Interfaces;
 using FakedOutShop.Infrastructure.Cosmos;
+using FakedOutShop.Infrastructure.Repositories;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace FakedOutShop.Infrastructure
 {
@@ -10,8 +13,12 @@ namespace FakedOutShop.Infrastructure
   {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+      services.Configure<CosmosOptions>(configuration.GetSection("CosmosDb"));
+
       CosmosClientProvider.ProvideCosmosClient(services, configuration);
+
       services.AddScoped<IUserService, UserService>();
+      services.AddScoped<IUserRepository, UserRepository>();
 
       return services;
     }
