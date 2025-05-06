74ba34b499204f522c0a2c98156c05b9247c48f5
yurights <yuriibpk@gmail.com>
2025-05-06 16:33:58 +0300

    upd: translate

--- apps/frontend/libs/feature-home/src/lib/feature-home/feature-home.component.ts
+++ apps/frontend/libs/feature-home/src/lib/feature-home/feature-home.component.ts
@@ -1,12 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ButtonComponent } from '@anx-store/shared/ui';
+import { TranslocoService, TranslocoDirective } from '@jsverse/transloco';
 
 @Component({
   selector: 'lib-feature-home',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, TranslocoDirective],
   templateUrl: './feature-home.component.html',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  // styles: [':host {display: flex;}'],
 })
-export class FeatureHomeComponent {}
+export class FeatureHomeComponent {
+  private readonly translocoService = inject(TranslocoService);
+}
