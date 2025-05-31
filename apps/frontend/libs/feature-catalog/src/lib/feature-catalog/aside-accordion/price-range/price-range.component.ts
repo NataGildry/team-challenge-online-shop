4dc774649d62d2e9f4b5ce0b68f7e5d8a3c78e84
yurights <yuriibpk@gmail.com>
2025-05-31 20:36:32 +0300

    test scss

--- apps/frontend/libs/feature-catalog/src/lib/feature-catalog/aside-accordion/price-range/price-range.component.ts
+++ apps/frontend/libs/feature-catalog/src/lib/feature-catalog/aside-accordion/price-range/price-range.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
 
 @Component({
   selector: 'lib-price-range',
-  imports: [CommonModule],
+  imports: [MatSliderModule],
   templateUrl: './price-range.component.html',
+  styleUrl: './price-range.component.scss',
 })
 export class PriceRangeComponent {
   protected readonly min = 1;
-  protected readonly max = 1000;
+  protected readonly max = 1500;
 
-  // protected inputRange(event: any): void {
-  //   console.log(event);
-  // }
+  protected lower = signal(100);
+  protected higher = signal(500);
+
+  protected inputLower(event: Event): void {
+    if (event.target instanceof HTMLInputElement) {
+      this.lower.set(Number.parseInt(event.target.value));
+    }
+  }
+  protected inputHigher(event: Event): void {
+    if (event.target instanceof HTMLInputElement) {
+      this.higher.set(Number.parseInt(event.target.value));
+    }
+  }
 }
