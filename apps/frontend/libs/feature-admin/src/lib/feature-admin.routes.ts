d4cde8d558f0204df33df4334a7641fff39cc9ca
yurights <yuriibpk@gmail.com>
2025-06-12 09:26:44 +0300

    chore: added add-product component

--- apps/frontend/libs/feature-admin/src/lib/feature-admin.routes.ts
+++ apps/frontend/libs/feature-admin/src/lib/feature-admin.routes.ts
@@ -1,9 +1,14 @@
 import { Route } from '@angular/router';
 import { FeatureAdminComponent } from './feature-admin/feature-admin.component';
+import { AddProductComponent } from './feature-admin/add-product/add-product.component';
 
 export const featureAdminRoutes: Route[] = [
   {
     path: 'admin',
-    component: FeatureAdminComponent,
+    children: [
+      { path: '', component: FeatureAdminComponent },
+      { path: 'add-product', component: AddProductComponent },
+      { path: 'edit-product/:id', component: AddProductComponent },
+    ],
   },
 ];
---
+++ apps/frontend/libs/feature-admin/src/lib/feature-admin/add-product/add-product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-add-product',
+  imports: [CommonModule],
+  templateUrl: './add-product.component.html',
+  styleUrl: './add-product.component.css',
+})
+export class AddProductComponent {}
