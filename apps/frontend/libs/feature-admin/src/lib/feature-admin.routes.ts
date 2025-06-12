fc7219a488cab4c7952119776683246cf12fbaae
yurights <yuriibpk@gmail.com>
2025-06-12 09:03:45 +0300

    chore: admin feature added

---
+++ apps/frontend/libs/feature-admin/src/lib/feature-admin.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from '@angular/router';
+import { FeatureAdminComponent } from './feature-admin/feature-admin.component';
+
+export const featureAdminRoutes: Route[] = [
+  {
+    path: 'admin',
+    component: FeatureAdminComponent,
+  },
+];
---
+++ apps/frontend/libs/feature-admin/src/lib/feature-admin/feature-admin.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-feature-admin',
+  templateUrl: './feature-admin.component.html',
+})
+export class FeatureAdminComponent {}
