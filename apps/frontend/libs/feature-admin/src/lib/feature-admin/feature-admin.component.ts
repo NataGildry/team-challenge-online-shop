bf44da180834c576bab64554a75f391b3b9119a3
yurights <yuriibpk@gmail.com>
2025-06-15 21:33:05 +0300

    chore: icons set expanded

--- apps/frontend/libs/feature-admin/src/lib/feature-admin/feature-admin.component.ts
+++ apps/frontend/libs/feature-admin/src/lib/feature-admin/feature-admin.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { ButtonComponent } from '@anx-store/shared/ui';
+import { angle, ButtonComponent, IconComponent ,
+  dashboard,
+  questionMark,
+  exit,
+  iconBasket,
+} from '@anx-store/shared/ui';
 
 @Component({
   selector: 'lib-feature-admin',
-  imports: [RouterOutlet, RouterLink, ButtonComponent],
+  imports: [RouterOutlet, RouterLink, ButtonComponent, IconComponent],
   templateUrl: './feature-admin.component.html',
 })
-export class FeatureAdminComponent {}
+export class FeatureAdminComponent {
+  protected readonly dashboardIcon = dashboard;
+  protected readonly productBasketIcon = iconBasket;
+  protected readonly questionMarkIcon = questionMark;
+  protected readonly exitIcon = exit;
+  protected readonly angleIcon = angle;
+}
