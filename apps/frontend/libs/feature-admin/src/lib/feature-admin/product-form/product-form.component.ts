712869c41f608e0846c97d9e1f52c5826e045565
yurights <yuriibpk@gmail.com>
2025-06-20 10:40:48 +0300

    chore: form inputs added

--- apps/frontend/libs/feature-admin/src/lib/feature-admin/product-form/product-form.component.ts
+++ apps/frontend/libs/feature-admin/src/lib/feature-admin/product-form/product-form.component.ts
@@ -1,7 +1,88 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  inject,
+  signal,
+} from '@angular/core';
+import { SelectComponent } from './select/select.component';
+import { ColorPickerComponent, IconComponent , FabricColors , angle } from '@anx-store/shared/ui';
+import { NgClass } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { tap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'lib-product-form',
+  imports: [
+    SelectComponent,
+    ColorPickerComponent,
+    IconComponent,
+    NgClass,
+    ReactiveFormsModule,
+  ],
   templateUrl: './product-form.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductFormComponent {}
+export class ProductFormComponent {
+  private readonly destroyRef = inject(DestroyRef);
+
+  protected readonly selectedColors = signal<string[]>(['color']);
+  protected readonly angleIcon = angle;
+
+  protected isColorPickerOpen = signal<boolean>(false);
+
+  protected statusOptions = [
+    { name: 'Available', value: 'unavailable' },
+    { name: 'Unavailable', value: 'unvailable' },
+  ];
+
+  protected categoryOptions = [
+    { name: 'All', value: 'all' },
+    { name: 'Armchairs and Sofas', value: 'armchairsandsofas' },
+    { name: 'Beds and Futons', value: 'bedsandfutons' },
+    { name: 'Chairs and Semi-chairs', value: 'chairsandsemichairs' },
+    { name: 'Kids Furniture', value: 'kidsfurniture' },
+  ];
+  protected readonly materialOptions = [
+    { name: 'Cotton', value: 'cotton' },
+    { name: 'Linen', value: 'linen' },
+    { name: 'Polyester', value: 'polyester' },
+    { name: 'Velvet', value: 'velvet' },
+    { name: 'Leather', value: 'leather' },
+    { name: 'Microfiber', value: 'microfiber' },
+    { name: 'Chenille', value: 'chenille' },
+    { name: 'Vinyl', value: 'vinyl' },
+  ];
+
+  protected colorsList = Object.entries(FabricColors).map(([k, v]) => ({
+    name: k,
+    hexCode: v,
+  }));
+
+  protected form = new FormGroup({
+    colorControl: new FormControl(),
+  });
+
+  public constructor() {
+    this.form
+      .get('colorControl')
+      ?.valueChanges.pipe(
+        tap((colors: string[]) => {
+          const newColors = colors.map(
+            (hex: string) =>
+              this.colorsList.find((el) => el.hexCode === hex)?.name
+          );
+          this.selectedColors.set(
+            newColors.filter((el) => typeof el === 'string')
+          );
+        }),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe();
+  }
+
+  protected toggleAccordion(): void {
+    this.isColorPickerOpen.set(!this.isColorPickerOpen());
+  }
+}
---
+++ apps/frontend/libs/feature-admin/src/lib/feature-admin/product-form/select/select.component.ts
@@ -0,0 +1,78 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  forwardRef,
+  inject,
+  input,
+  signal,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import {
+  angle,
+  IconComponent,
+  SharedIcon,
+  ClickOutsideDirective,
+} from '@anx-store/shared/ui';
+import { TranslocoDirective, TranslocoService } from '@jsverse/transloco';
+
+export interface SelectOption {
+  name: string;
+  value: string;
+}
+
+@Component({
+  selector: 'lib-select',
+  imports: [ClickOutsideDirective, IconComponent, TranslocoDirective],
+  templateUrl: './select.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true,
+    },
+  ],
+})
+export class SelectComponent implements ControlValueAccessor {
+  protected readonly isOpen = signal(false);
+  protected readonly isDisabled = signal(false);
+  protected readonly value = signal<string>('popularity');
+  protected readonly selected = computed(() => {
+    const option = this.options().find((el) => el.value === this.value());
+    return option?.name ?? this.options()[0].name;
+  });
+
+  private readonly translocoService = inject(TranslocoService);
+
+  public readonly options = input<SelectOption[]>([]);
+
+  private onChange?: (value: string) => void;
+  private onTouched?: () => void;
+
+  protected readonly angle: SharedIcon = angle;
+
+  protected selectOption(item: SelectOption): void {
+    this.value.set(item.value);
+    this.isOpen.set(false);
+    this.onChange?.(this.value());
+    this.onTouched?.();
+  }
+
+  public writeValue(value: string): void {
+    this.value.set(value);
+  }
+
+  public registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  public registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    this.isDisabled.set(isDisabled);
+  }
+}
